eed78afd9032b51c543a583a3d066048fbaa9d25
moein <moeinkolivand97@gmail.com>
2026-08-17 15:25:26 +0330

    add wallet consumer user registration

---
+++ wallet/src/main/java/com/tutorial/wallet/wallet/consumer/UserRegistrationConsumer.java
@@ -0,0 +1,34 @@
+package com.tutorial.wallet.wallet.consumer;
+
+
+import com.tutorial.shared.user.events.UserRegisteredEvent;
+import com.tutorial.wallet.wallet.WalletService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRegistrationConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(UserRegistrationConsumer.class);
+    private final WalletService walletService;
+
+    @Autowired
+    public UserRegistrationConsumer(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @KafkaListener(topics = "user-registered-topic", groupId = "wallet-group")
+    public void handleUserRegistration(UserRegisteredEvent event) {
+        log.info("Received event: User ID {} registered. Creating default wallet...", event.getUserId());
+
+        try {
+             walletService.createDefaultWallet(event.getUserId());
+            log.info("Default wallet created for user: {}", event.getUserId());
+        } catch (Exception e) {
+            log.error("Failed to create wallet for user {}: {}", event.getUserId(), e.getMessage());
+        }
+    }
+}
