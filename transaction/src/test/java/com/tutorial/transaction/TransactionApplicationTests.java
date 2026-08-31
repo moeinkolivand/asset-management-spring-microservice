6fe47e20a842166592ff60273c7b0fb2144ad8ed
moein <moeinkolivand97@gmail.com>
2026-09-01 00:26:08 +0330

    disabling contextLoads on test

--- transaction/src/test/java/com/tutorial/transaction/TransactionApplicationTests.java
+++ transaction/src/test/java/com/tutorial/transaction/TransactionApplicationTests.java
@@ -2,12 +2,14 @@ package com.tutorial.transaction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TransactionApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
